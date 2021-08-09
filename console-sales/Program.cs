5639ebd011e86c92a9ebadefa2c079a221a72346
Luís <lg.apone@uol.com.br>
2021-08-08 22:30:22 -0300

    release/1.0

---
+++ console-sales/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace console_sales
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Sale> listSales = new();
+            List<string> listSeller = new();
+
+            string seller;
+            DateTime dateSale;
+            decimal valueSale;
+
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine($"Enter {i}° seller name:");
+                seller = Console.ReadLine();
+
+                if (listSeller.Contains(seller))
+                {
+                    Console.WriteLine("Seller alredy inputted!");
+                    i--;
+                    continue;
+                }
+
+                listSeller.Add(seller);
+            }
+
+            while (true)
+            {
+                Console.WriteLine("Enter seller name:");
+                seller = Console.ReadLine();
+
+                if (!listSeller.Contains(seller))
+                {
+                    Console.WriteLine("Invalid Seller");
+                    continue;
+                }
+
+                Console.WriteLine("Enter customer name:");
+                string customer = Console.ReadLine();
+
+                Console.WriteLine("Enter date of sale:");
+                string date = Console.ReadLine();
+                dateSale = new();
+                try
+                {
+                    dateSale = DateTime.Parse(date);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Invalid input date");
+                    continue;
+                }
+
+                Console.WriteLine("Enter sale item name:");
+                string item = Console.ReadLine();
+
+                Console.WriteLine("Enter sale value:");
+                string value = Console.ReadLine();
+                try
+                {
+                    valueSale = decimal.Parse(value);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Invalid input value");
+                    continue;
+                }
+
+                listSales.Add(new Sale(seller, customer, dateSale, item, valueSale));
+
+                foreach (Sale sale in listSales.OrderByDescending(x => x.SaleValue).ToList()) {
+                    Console.WriteLine(sale.ToString());
+                }
+            }
+        }
+    }
+}
---
+++ console-sales/Sale.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace console_sales
+{
+    public class Sale
+    {
+        public Sale(string sellerName, string customerName, DateTime dateSale, string saleItemName, decimal saleValue)
+        {
+            SellerName = sellerName;
+            CustomerName = customerName;
+            DateSale = dateSale;
+            SaleItemName = saleItemName;
+            SaleValue = saleValue;
+        }
+
+        public string SellerName { get; }
+        public string CustomerName { get; }
+        public DateTime DateSale { get; }
+        public string SaleItemName { get; }
+        public decimal SaleValue { get; }
+
+        public override string ToString()
+        {
+            return $"Seller={SellerName};Customer={CustomerName};Date={DateSale.ToShortDateString()};Item={SaleItemName};Value={SaleValue};";
+        }
+    }
+}
